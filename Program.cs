2cf7e03943e49e954e8784177bc3c736872fbe76
Luizhnrs <luizhnrsg@gmail.com>
2026-04-21 21:44:21 -0300

    FEAT: String de conexão implementada

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Connection string for SQL Server
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? "Server=mssql,1433;Initial Catalog=MyDatabase;User ID=sa;Password=Your_password123;TrustServerCertificate=True";
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<resting_dotnet.Context.ClientDbContext>(options =>
+    options.UseSqlServer(connectionString));
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
 
 var app = builder.Build();
 
